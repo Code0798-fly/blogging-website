386c9400209643a38a0ccf9334b8d2e1e9331b0c
Code0798-fly <codeblocksplus@gmail.com>
2026-02-23 21:04:01 +0530

    Wrap App component with HelmetProvider

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,25 @@ import { BlogPost } from './components/BlogPost';
 import { About } from './components/About';
 import { Contact } from './components/Contact';
 import { CategoryPage } from './components/CategoryPage';
+import { HelmetProvider } from 'react-helmet-async';
 
 export default function App() {
   return (
-    <div>
-      {/* Example Header Navigation */}
-     
-
-      {/* Routes */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/blogs" element={<BlogListing />} />
-        <Route path="/blog/:slug" element={<BlogPost />} />
-        <Route path="/category/:category" element={<CategoryPage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </div>
+    <HelmetProvider>
+      <div>
+        {/* Example Header Navigation */}
+       
+  
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blogs" element={<BlogListing />} />
+          <Route path="/blog/:slug" element={<BlogPost />} />
+          <Route path="/category/:category" element={<CategoryPage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </HelmetProvider>
   );
 }
